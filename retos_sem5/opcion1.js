1e107cda393f3ca53a9148462b6a2558bfe868e8
Graciela <64287161+gracetyk@users.noreply.github.com>
2021-09-26 21:16:19 -0500

    Delete opcion1.js

--- retos_sem5/opcion1.js
+++
@@ -1,8 +0,0 @@
-let L=+prompt("Cuantos litros de leche entregará?");
-let PG=+prompt("ingrese el precio de 1 galon de leche");
-
-let TG=L/3785; //galones
-let GA=TG*PG;
-
-console.log(`Usted venderá ${TG} galones
-             deberá recibir ${GA} soles por la venta`);
